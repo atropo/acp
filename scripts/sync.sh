036f6b8623a750afeccaf48cd035c91c85f5aefd
Atropo <atropo@gmail.com>
2016-12-12 16:50:05 +0100

    Scripts per il sync online

---
+++ scripts/sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Syncing with remote..."
+echo "*************************************************************************"
+echo "*                              ATTENZIONE                               *"
+echo "* Eventuali file caricati non via rsync potrebbero essere cancellati.   *"
+echo "*                                                                       *"
+echo "************************************************************************"
+rsync --delete-after -vaz -e ssh --exclude ".git" app faula:acp/
+#nonino77
