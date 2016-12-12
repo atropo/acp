4b6b01c6807036671b7bb4ec5cfa083d5f6db83f
Atropo <atropo@gmail.com>
2016-12-12 19:04:01 +0100

    Aggiornato script di upload

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,5 +5,5 @@ echo "*                              ATTENZIONE                               *"
 echo "* Eventuali file caricati non via rsync potrebbero essere cancellati.   *"
 echo "*                                                                       *"
 echo "************************************************************************"
-rsync --delete-after -vaz -e ssh --exclude ".git" app faula:acp/
+rsync --delete-after -vaz -e ssh --exclude ".git" ../app/ faula:acp/
 #nonino77
