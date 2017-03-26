4f2bf954dbe0037508815286d8ca226e0435ff7d
Atropo <atropo@gmail.com>
2017-03-26 14:23:23 +0200

    Carousel homepage con scroll random

--- app/home/home.js
+++ app/home/home.js
@@ -9,7 +9,22 @@ angular.module('myApp.home', ['ngRoute'])
   });
 }])
 
-.controller('homeCtrl', ['$rootScope', function($rootScope) {
+.controller('homeCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
 
 	$rootScope.section="home";
+	var campiPratica=[
+		{img:"faula.jpg", nome:"La Faula Golf Club"},
+		{img:"bosco.jpg", nome:"Golf Club Il Bosco"},
+		{img:"abano.jpg", nome:"Golf Club Abano"},
+		{img:"ancora.jpg", nome:"Golf Club Ancora"},
+		{img:"brolo.jpg", nome:"Golf Club Brolo Bassano"},
+		{img:"musella.jpg", nome:"Golf Club Musella"},
+		{img:"noghere.jpg", nome:"Golf Club Le Noghere"},
+		{img:"polo.jpg", nome:"Golf Club Marco Polo"},
+		{img:"conegliano.jpg", nome:"Golf Club Ville di Conegliano "},
+		{img:"corradina.jpg", nome:"Golf Club la Corradina "},
+		{img:"5laghi.jpg", nome:"Golf Club 5 Laghi "}
+	];
+
+	$scope.campiPratica = _.shuffle(campiPratica)
 }]);
